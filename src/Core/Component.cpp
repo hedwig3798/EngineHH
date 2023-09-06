9d6296cc1cd4070c193f16f2e0128683be29362f
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2023-09-06 13:57:30 +0900

    엔진 초기화

--- src/Core/Component.cpp
+++ src/Core/Component.cpp
@@ -1,13 +1,1 @@
 #include "Component.h"
-
-Component::Component(CALL_ORDER _order, std::string _name, Object* owner)
-	: callOrder(_order)
-	, name(_name)
-	, owner(owner)
-{
-}
-
-Component::~Component()
-{
-
-}
---
+++ src/lib/GameEnigneHeader/MonoBehaviour.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Component.h"
+
+class MonoBehaviour
+	: public Component
+{
+public:
+	// 持失切 社瑚切
+	MonoBehaviour() {};
+	virtual ~MonoBehaviour() {};
+};
