dd041b4b26ff0c254e424658d93e1668b082934d
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2023-10-11 10:57:44 +0900

    머지 테스트용 커밋
    머지 테스트용 커밋

--- src/D3D/DemoProcess.cpp
+++ src/D3D/DemoProcess.cpp
@@ -79,6 +79,6 @@ void DemoProcess::Render()
 	this->axes->Render(graphicsEngine);
 	TCHAR* explain = (TCHAR*)(L"W, S : 카메라 전방, 후방 이동\nA, D : 카메라 좌우 이동\n화살표 : 카메라 회전");
 	this->graphicsEngine->WriteText(10, 10, COLORS::White, explain);
-
 	this->graphicsEngine->endDraw();
+	int test = 0;
 }
